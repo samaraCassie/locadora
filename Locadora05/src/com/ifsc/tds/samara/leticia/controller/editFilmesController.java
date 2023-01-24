28a430cf786e3ab315b4a2aef80724ff5a3ebf38
samaraCassie <samaracassie@outlook.com>
2023-01-24 19:35:45 -0300

    Create editFilmesController.java

---
+++ Locadora05/src/com/ifsc/tds/samara/leticia/controller/editFilmesController.java
@@ -0,0 +1,131 @@
+package com.ifsc.tds.samara.leticia.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.ifsc.tds.samara.leticia.entity.Filme;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class editFilmesController implements Initializable{
+	@FXML
+    private AnchorPane paneMain;
+
+    @FXML
+    private GridPane paneGrid;
+
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private Label lblFone;
+
+    @FXML
+    private TextField txtFone;
+    
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private HBox boxButton;
+
+    @FXML
+    private Button btnOK;
+
+    @FXML
+    private Button btnCancela;
+
+	private Stage janelaClienteEdit;
+
+	private Filme cliente;
+
+	private boolean OnClickOk = false;
+
+	@FXML
+	void onClickBtnCancela(ActionEvent event) {
+		this.getJanelaClienteEdit().close();
+	}
+
+	@FXML
+	void onClickBtnOK(ActionEvent event) {
+		if (validarCampos()) {
+			this.cliente.setNome(this.txtNome.getText());
+			this.cliente.setFone(this.txtFone.getText());
+			this.cliente.setEmail(this.txtEmail.getText());
+
+			this.OnClickOk = true;
+			this.getJanelaClienteEdit().close();
+		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+	public Stage getJanelaClienteEdit() {
+		return janelaClienteEdit;
+	}
+
+	public void setJanelaClienteEdit(Stage janelaClienteEdit) {
+		this.janelaClienteEdit = janelaClienteEdit;
+	}
+
+	public void populaTela(Filme filme) {
+		this.cliente = filme;
+
+		this.txtNome.setText(filme.getNome());
+		this.txtFone.setText(filme.getFone());
+		this.txtEmail.setText(filme.getEmail());
+	}
+
+	public boolean isOkClick() {
+		return OnClickOk;
+	}
+
+	private boolean validarCampos() {
+		String avisoERRO = new String();
+
+		if (this.txtNome.getText() == null || this.txtNome.getText().trim().length() == 0) {
+			avisoERRO += "Verificar o nome!\n";
+		}
+
+		if (this.txtFone.getText() == null || this.txtFone.getText().trim().length() == 0) {
+			avisoERRO += "Verificar o telefone!\n";
+		}
+		
+		if (this.txtEmail.getText() == null || this.txtEmail.getText().trim().length() == 0) {
+			avisoERRO += "Verificar o email!\n";
+		}
+
+		if (avisoERRO.length() == 0) {
+			return true;
+		} else {
+			Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.initOwner(this.janelaClienteEdit);
+			alerta.setTitle("Dados invalidos!");
+			alerta.setHeaderText("Tente:");
+			alerta.setContentText(avisoERRO);
+			alerta.showAndWait();
+
+			return false;
+		}
+	}
+}
+
